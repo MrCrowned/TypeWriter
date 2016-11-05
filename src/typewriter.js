8b7cc381d092fe8fd03caba2b085c84d256e8df4
Dominic M Brause <dom.brause@gmail.com>
2016-11-05 20:29:03 +0000

    + ES6 constructor method + arrow functions What is cb? Current ???

--- src/typewriter.js
+++ src/typewriter.js
@@ -1,33 +1,25 @@
 /*
  * typewriter.js
+ * An ES6 Port of the original typewriter.js
  *
  * Copyright 2014, Connor Atherton - http://connoratherton.com/
  * Released under the MIT Licence
  * http://opensource.org/licenses/MIT
  *
+ * 
+ * 
  * Github:  http://github.com/ConnorAtherton/typewriter
  */
+'use strict';
 
-;(function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(factory);
-  } else if (typeof exports === 'object') {
-    // CommonJS
-    module.exports = factory();
-  } else {
-    // Browser globals
-    root.Typewriter = factory();
-  }
-}(this, function factory(exports) {
-  'use strict';
+class Typewriter {
 
-   var TypeWriter = TypeWriter || function (element, opts) {
+   constructor(element, opts) {
      if (!element) throw new Error('A selector or element must be specified');
      if (!opts.text) throw new Error('TypeWriter needs text to type');
 
      var options = {
-       element: !(element instanceof Node) ? document.querySelector(selector) : element,
+       element: document.querySelector(selector) || element,
        text: opts.text,
        words: opts.words || false,
        interval: opts.interval || 'human',
@@ -35,25 +27,25 @@
        upperBound: opts.upperBound || 200
      },
 
-     randomIntFromInterval = function(min, max) {
+     randomIntFromInterval = (min, max) => {
        return Math.floor(Math.random() * (max - min + 1) + min);
      },
 
-     isNumber = function(n) {
-       return !isNaN(parseFloat(n)) && isFinite(n);
+     isNumber = (number) => {
+       return !isNaN(parseFloat(number)) && isFinite(number);
      },
 
-     getIntervalSpeed = function() {
+     getIntervalSpeed = () => {
        if (isNumber(options.interval)) return options.interval;
 
        return randomIntFromInterval(options.lowerBound, options.upperBound);
      },
 
-     typeByLettersConstantInterval = function(cb) {
+     typeByLettersConstantInterval = (cb) => {
        var numberOfLetters = options.text.length,
            currentPosition = 0;
 
-       var interval = window.setInterval(function() {
+       var interval = window.setInterval(() => {
          if (currentPosition === numberOfLetters) {
            window.clearInterval(interval);
 
@@ -65,14 +57,14 @@
        }, getIntervalSpeed());
      },
 
-     typeByLettersRandomisedInterval = function(cb) {
+     typeByLettersRandomisedInterval = (cb) => {
        var numberOfLetters = options.text.length,
            currentPosition = 0;
 
        repeat(numberOfLetters, currentPosition, cb);
      },
 
-     repeat = function(numberOfLetters, currentPosition, cb) {
+     repeat = (numberOfLetters, currentPosition, cb) => {
        if (numberOfLetters === 0) return cb && cb.call(window);
 
        var interval = getIntervalSpeed.call(),
@@ -80,18 +72,18 @@
 
        options.element.innerHTML += options.text[currentPosition];
 
-       timer = setTimeout(function() {
+       timer = setTimeout(() => {
          numberOfLetters--; currentPosition++;
          repeat(numberOfLetters, currentPosition, cb);
        }, interval);
      },
 
-     typeByWords = function(cb) {
-       var words = options.text.split(' '),
+     typeByWords = (cb) => {
+       let words = options.text.split(' '),
            numberOfWords = words.length,
            currentPosition = 0;
 
-       var interval = window.setInterval(function() {
+       let interval = window.setInterval(() => {
          if (currentPosition === numberOfWords) {
            window.clearInterval(interval);
 
@@ -103,14 +95,11 @@
        }, getIntervalSpeed());
      };
 
-     this.type = function(cb) {
+     this.type = (cb) => {
        options.words ? typeByWords(cb) :
            isNumber(options.interval) ? typeByLettersConstantInterval(cb) :
                                         typeByLettersRandomisedInterval(cb);
-     };
+     }
 
-   };
-
-   return TypeWriter;
-
-}));
+  }
+}
